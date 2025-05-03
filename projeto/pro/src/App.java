76144cb93e5e2af42756142dad31eb2b42500a9b
jandsonanjos <jandsonanjos@hotmail.com>
2025-05-03 17:16:40 -0300

    alterando a função

--- projeto/pro/src/App.java
+++ projeto/pro/src/App.java
@@ -7,6 +7,13 @@ public class App {
         livroTDD = 59.90;
 
         double soma = livroJava8 + livroTDD;
+        if(soma < 150){
+            System.out.println("Seu estoque está muito baixo!");
+        }else if(soma >=2000){
+            System.out.println("Seu estoque está muito alto!");
+        }else{
+            System.out.println("Seu estoque está bom!");
+        }
      
         System.out.println("O total em estoque é: $"+soma);
     }
