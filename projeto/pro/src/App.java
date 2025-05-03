71e9dcbdd4996db5471ed9c92eb4d82b05cebc83
jandsonanjos <jandsonanjos@hotmail.com>
2025-05-03 11:46:28 -0300

    first commit project

---
+++ projeto/pro/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double livroJava8;
+        double livroTDD;
+
+        livroJava8 = 59.90;
+        livroTDD = 59.90;
+
+        double soma = livroJava8 + livroTDD;
+     
+        System.out.println("O total em estoque é: $"+soma);
+    }
+}
