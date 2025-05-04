e9c2722972cf88e7c1f7df317178e804787cd3c7
jandsonanjos <jandsonanjos@hotmail.com>
2025-05-04 01:57:16 -0300

    adicionando emoji

--- projeto/pro/src/App.java
+++ projeto/pro/src/App.java
@@ -1,14 +1,12 @@
+//class running
 public class App {
     public static void main(String[] args) throws Exception {
-        
         double soma = 0;
-        
         for (double i = 0; i < 35; i++){
             soma +=59.90;
         }
 
         System.out.println("O total em estoque é $" +soma);
-
         if(soma < 150){
             System.out.println("Seu estoque está muito baixo!");
         }else if(soma >=2000){
